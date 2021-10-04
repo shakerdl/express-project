1c5602595189c04e9781685236b7ca23939daf7d
shakerdl <=>
2021-10-04 12:13:00 +0300

    env_file

--- app.js
+++ app.js
@@ -2,24 +2,35 @@
 
  const express = require("express");
  const app = express();
+ require("dotenv").config();
 
- app.get('/',(req,res)=>{    // in the method we are get the root and send it back the h1 via respond
-     console.log(req.url); // it only show us the "/" on the console
-     res.send("<h1>helo</h1>");
- }); // determine the content-type automatically
-app.get('/old',(req,res)=>{
-    res.redirect(301,'/new')
-});
+let port = process.env.PORT;
+let host = process.env.HOST;
 
-app.get('/new',(req,res)=>{  
-   
-    res.send("<h2>new</h2>");
-}); 
 
- app.listen(3000,(err)=> {
+
+ app.get('/shakerdl',(req,res)=>{    // in the method we are get the root and send it back the h1 via respond
+    //  console.log(req.headers);
+    console.log(req.url);
+    console.log(req.path);
+    console.log(req.ip);
+    console.log(req.method);
+    console.log(req.query); 
+    console.log(req.params); // here I"ll take  app.get("/user/45") or app.get("/user/:id")  req.params.id
+
+    
+
+
+
+
+     res.status(404).end();
+ }); 
+
+ app.listen(port,host,(err)=> {
     if (err) {
         console.log('there is a problem',err);
         return;
     }
-    console.log('listening on port 3000');
+    console.log(`server is lisiting ${host}:${port}`); // I have to use backticket
  });
+
